1b68bb4fb25c08c26dd0fb805fbfc9d1fde4c8e1
Henrique H. da Silva Fortes <Henrique Fortes>
2021-11-26 00:00:41 -0300

    Add project files.

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using MinimalApiAuth.Models;
+
+namespace MinimalApiAuth.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>
+            {
+                new User {Id = 1, Username = "batman", Password = "batman123"},
+                new User {Id = 2, Username = "robin", Password = "robin123"}
+            };
+            return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == password).FirstOrDefault();
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace MinimalApiAuth
+{
+    public class Settings
+    {
+        public static string Secret = "xGUQxPPZV21ZArvhRpylnU2Lp6skAUCpqewqeqw";
+    }
+}
